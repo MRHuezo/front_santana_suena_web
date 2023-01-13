22874ba0d3862cd4d4d8996f141d493270690c59
AldoChagollan <aldochagollan@outlook.com>
2023-01-12 19:11:49 -0600

    home estructura

--- src/Views/Home/PrincipalHome/index.jsx
+++ src/Views/Home/PrincipalHome/index.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import React from "react";
+import { Box } from "@mui/system";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-function PrincipalHome() {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./styles.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+export default function PrincipalHome() {
   return (
-    <Box
-      border={1}
-      sx={{
-        height: "50vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      Banner principal
+    <Box sx={{height: "60vh"}}>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Banner 1</SwiperSlide>
+        <SwiperSlide>Banner 2</SwiperSlide>
+        <SwiperSlide>Banner 3</SwiperSlide>
+      </Swiper>
     </Box>
-  )
+  );
 }
-
-export default PrincipalHome
