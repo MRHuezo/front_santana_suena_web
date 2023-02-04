bd9d3dde5bbfa0868e200f2ce9df2dee38d3e46f
AldoChagollan <aldochagollan@outlook.com>
2023-02-03 22:01:34 -0600

    query general participantes

--- src/Views/Admin/Participantes/index.jsx
+++ src/Views/Admin/Participantes/index.jsx
@@ -3,15 +3,20 @@ import BuscarParticiante from './BuscarParticipante'
 import TablaParticipantes from './TablaParticipantes'
 import { Box } from '@mui/material'
 import TopNavAdmin from '../../../Components/Navbar/TopNavAdmin'
+import { useState } from 'react'
+import { useDebounce } from 'use-debounce';
 
 function Participantes() {
+  const [search, setSearch] = useState("");
+  const [value] = useDebounce(search, 500);
+
   return (
     <div>
       <TopNavAdmin title="Participantes" />
       si es sede principal buscar por todos si no nada mas su sede
-      <BuscarParticiante />
+      <BuscarParticiante search={search} setSearch={setSearch} />
       <Box my={2} />
-      <TablaParticipantes />
+      <TablaParticipantes search={value} />
     </div>
   )
 }
